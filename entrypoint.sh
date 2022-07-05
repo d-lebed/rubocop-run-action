89159b47c1d000f0e1e43a1e079832d8ddfd26fa
Dmitry Lebed <209805+d-lebed@users.noreply.github.com>
2022-07-05 13:41:10 +0300

    Test inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 
 # export preserve_exitcode=${INPUT_PRESERVER_EXITCODE:-origin/master}
 
+echo "INPUTS_TEST_STRING ${INPUTS_TEST_STRING}"
+echo "INPUTS_TEST_FIRST ${INPUTS_TEST_FIRST}"
+echo "INPUTS_TEST_OTHER ${INPUTS_TEST_OTHER}"
+
 bundle exec rubocop $@
+
+exit 0
