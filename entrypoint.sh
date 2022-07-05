acab87515791a099c6b0516dd73ac57c849445da
Dmitry Lebed <209805+d-lebed@users.noreply.github.com>
2022-07-05 13:48:14 +0300

    Test inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,22 @@ echo "INPUT_TEST_STRING ${INPUT_TEST_STRING}"
 echo "INPUT_TEST_FIRST ${INPUT_TEST_FIRST}"
 echo "INPUT_TEST_OTHER ${INPUT_TEST_OTHER}"
 
+if [[ ${INPUT_TEST_FIRST} == "true" ]]; then
+  echo "INPUT_TEST_FIRST \"true\""
+fi
+
+if [[ ${INPUT_TEST_FIRST} == true ]]; then
+  echo "INPUT_TEST_FIRST 'true'"
+fi
+
+if [[ ${INPUT_TEST_OTHER} == "false" ]]; then
+  echo "INPUT_TEST_OTHER \"false\""
+fi
+
+if [[ ${INPUT_TEST_OTHER} == false ]]; then
+  echo "INPUT_TEST_OTHER false"
+fi
+
 bundle exec rubocop $@
 
 exit 0
