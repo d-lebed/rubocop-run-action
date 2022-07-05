1e675a2f5d547e86aba861d8aae69365b84d4028
Dmitry Lebed <209805+d-lebed@users.noreply.github.com>
2022-07-05 13:56:49 +0300

    Exitcode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,6 @@
 #!/usr/bin/env sh
 
-# export preserve_exitcode=${INPUT_PRESERVER_EXITCODE:-origin/master}
-
-echo "INPUT_TEST_STRING ${INPUT_TEST_STRING}"
-echo "INPUT_TEST_FIRST ${INPUT_TEST_FIRST}"
-echo "INPUT_TEST_OTHER ${INPUT_TEST_OTHER}"
-
-if [[ ${INPUT_TEST_FIRST} == "true" ]]; then
-  echo "INPUT_TEST_FIRST \"true\""
-fi
-
-if [[ ${INPUT_TEST_FIRST} == true ]]; then
-  echo "INPUT_TEST_FIRST 'true'"
-fi
-
-if [[ ${INPUT_TEST_OTHER} == "false" ]]; then
-  echo "INPUT_TEST_OTHER \"false\""
-fi
-
-if [[ ${INPUT_TEST_OTHER} == false ]]; then
-  echo "INPUT_TEST_OTHER false"
-fi
-
 bundle exec rubocop $@
+EXITCODE=$?
 
-exit 0
+exit ($INPUT_PRESERVE_EXITCODE && $EXITCODE || 0)
