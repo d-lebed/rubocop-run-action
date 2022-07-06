5dbd6a7817475a97901318a4a8bc9e29f6084489
Dmitry Lebed <209805+d-lebed@users.noreply.github.com>
2022-07-06 14:26:09 +0300

    pr-4

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,9 @@
 
 RUNNER_WORKDIR=${INPUT_WORKDIR:-.}
 RUNNER_OPTIONS=${INPUT_OPTIONS:-"$@"}
-RUNNER_EXTRA_OPTIONS=""
 
 if $INPUT_RDJSON_FORMATTER; then
-  RUNNER_EXTRA_OPTIONS="${RUNNER_EXTRA_OPTIONS} --require /rubocop/lib/rdjson_formatter.rb"
+  RUNNER_OPTIONS="--require /rubocop/lib/rdjson_formatter.rb ${RUNNER_OPTIONS}"
 fi
 
 if [[ -z ${GITHUB_WORKSPACE} ]]; then
@@ -14,7 +13,7 @@ else
   cd ${GITHUB_WORKSPACE}/${RUNNER_WORKDIR}
 fi
 
-bundle exec rubocop ${RUNNER_EXTRA_OPTIONS} ${RUNNER_OPTIONS}
+bundle exec rubocop ${RUNNER_OPTIONS}
 EXITCODE=$?
 
 if $INPUT_PRESERVE_EXITCODE; then
